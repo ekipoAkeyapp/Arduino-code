c4bcecff5e94e58c6771a4efe1ecbd5e1b905ccf
ekipoAkeyapp <82879692+ekipoAkeyapp@users.noreply.github.com>
2021-05-04 12:29:42 +0200

    Update Codigo_Arduino.ino

--- Codigo_Arduino.ino
+++ Codigo_Arduino.ino
@@ -6,7 +6,6 @@ void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600); // begin serial communication
   
-
 }
 
 void loop() {
@@ -14,22 +13,22 @@ void loop() {
    int keyVal = analogRead(A0);
    Serial.println(keyVal);
 
-   if(keyVal == 1023){
+   if(keyVal >= 677 && keyVal <= 690){
     tone(8, notes[0]);
     } 
-   else if(keyVal >= 990 && keyVal <= 1010){
+   else if(keyVal >= 650 && keyVal <= 675){
      tone(8, notes[1]);
     }
-   else if(keyVal >= 505 && keyVal <= 515){
+   else if(keyVal >= 330 && keyVal <= 350){
      tone(8, notes[2]);
     }
-   else if(keyVal >= 800 && keyVal <= 900){
+   else if(keyVal >= 550 && keyVal <= 570){
      tone(8, notes[3]);
     }
-   else if(keyVal >= 5 && keyVal <= 15){
+   else if(keyVal >= 1 && keyVal <= 10){
      tone(8, notes[4]);
     }
-   else if(keyVal >= 35 && keyVal <= 50){
+   else if(keyVal >= 15 && keyVal <= 40){
      tone(8, notes[5]);
     }
    else{
